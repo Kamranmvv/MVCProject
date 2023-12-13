259aaa8f55379ab8b0d95d7840ce9620728b72fe
Kamran Mammadov <kamranmv1@gmail.com>
2023-12-13 13:20:35 +0400

    MVCProject created

---
+++ MVCProject/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCProject.Models;
+using System.Text.Json;
+
+namespace MVCProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            #region Model Based data transfer
+            //var products = new List<Product>
+            //{
+            //    new Product { Id = 1, Name= "Notebook 1", Quantity = 5},
+            //    new Product { Id = 2, Name= "Notebook 2", Quantity = 15},
+            //    new Product { Id = 3, Name= "Notebook 3", Quantity = 25}
+            //};
+            //return View(products);
+            #endregion
+            #region ViewBag
+            var products = new List<Product>
+            {
+                new Product { Id = 1, Name= "Notebook 1", Quantity = 5},
+                new Product { Id = 2, Name= "Notebook 2", Quantity = 15},
+                new Product { Id = 3, Name= "Notebook 3", Quantity = 25}
+            };
+            ViewBag.Products = products;
+
+            #endregion
+            
+            ViewData["products"] = products;
+            string data = JsonSerializer.Serialize(products);
+            TempData["products"] = data;
+            return RedirectToAction("Index2","Product");
+        }
+        public IActionResult Index2()
+        {
+            //var v1 = ViewBag.products;
+            //var v2 = ViewData["products"];
+            //var v3 = TempData["products"];
+
+            var data = TempData["products"].ToString();
+            List<Product> products = JsonSerializer.Deserialize<List<Product>>(data);
+            return View(); 
+        }
+    }
+
+}
---
+++ MVCProject/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCProject.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
